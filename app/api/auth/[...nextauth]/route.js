476d948f20975a1ea428ca89d4b25a3e682ff6bb
bstrat <benstratton100@gmail.com>
2023-05-19 17:48:05 -0500

    connected google-auth api and mongoDB for backend auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { connectToDB } from '@utils/database';
+
+console.log({
+    clientId: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+})
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    async session({ session }) {
+
+    },
+    async signIn({ profile }) {
+        try {
+            await connectToDB();
+
+            // Check if a user already exists
+
+            // If not, create a new user
+
+            return true;
+        } catch (err) {
+            console.log(err)
+            return false;
+        }
+    }
+})
+
+export { handler as GET, handler as POST };
