2a9ffb4a5a896425849d43ff0ed9bc0d9a3a9460
bstrat <benstratton100@gmail.com>
2023-06-02 08:52:22 -0400

    added verification and profile creation to auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from "@models/user";
 import { connectToDB } from '@utils/database';
 
 console.log({
@@ -23,8 +24,18 @@ const handler = NextAuth({
             await connectToDB();
 
             // Check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            })
 
             // If not, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
 
             return true;
         } catch (err) {
