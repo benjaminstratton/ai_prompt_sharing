c3a4224bbe51f22ecdacc03515c2a7e1ae561bf5
bstrat <benstratton100@gmail.com>
2023-06-06 21:50:20 -0400

    created profile page with get, update, and delete routes - added get and update to frontend

---
+++ app/profile/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fecthPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fecthPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
