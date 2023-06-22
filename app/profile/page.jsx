4bc4bb33ece797b4dba87cd65ac4ced823d6a16f
bstrat <benstratton100@gmail.com>
2023-06-22 13:23:56 -0400

    created delete function for full CRUD

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,7 +27,23 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE"
+        });
+
+        const filterPosts = myPosts.filter((p) => p._id !== post._id);
+
+        setMyPosts(filterPosts);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  };
 
   return (
     <Profile
